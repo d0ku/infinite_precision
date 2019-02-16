6c9e6dec59aa8733cef3e7c39a80b7220d063cec
d0ku <darthtyranus666666@gmail.com>
2019-02-17 00:24:52 +0100

    added tests for arithmetic, bit sign

--- inf_prec.py
+++ inf_prec.py
@@ -2,14 +2,12 @@
 Basic functions used in fraction arithmetic operations like greatest common
 divisor and lowest common denominator."""
 
-# TODO: gcd and lcd with negative numbers, consider.
-
-import cProfile
-
 
 def gcd(a: int, b: int) -> int:
     """Greatest common divisor, find highest number c such that
     a%c == b%c == 0"""
+    a = abs(a)
+    b = abs(b)
     if b > a:
         a, b = b, a
 
@@ -23,6 +21,8 @@ def lcd(a: int, b: int) -> int:
     """Lowest common denominator, find lowest number c such that
     c%a == c%b == 0
         lcd(a,b) = ab/gcd(a, b)"""
+    a = abs(a)
+    b = abs(b)
 
     return (a // gcd(a, b)) * b
 
@@ -30,15 +30,20 @@ def lcd(a: int, b: int) -> int:
 class Number:
     """Remember numerator and denominator, some functionalities about basic
     arithmethic operations."""
-    def __init__(self, numerator, denominator=1):
+    def __init__(self, numerator, denominator=1, negative=False):
         if denominator == 0:
             raise ValueError("Denominator can not be zero.")
+        if numerator < 0 or denominator < 0:
+            raise ValueError("Values used must be positive, use sign bit\
+                             instead.")
         self.numerator = numerator
         self.denominator = denominator
+        self.negative = negative
 
     def __str__(self):
-        return "{0}/{1} {2}".format(self.numerator, self.denominator,
-                                    self.numerator / self.denominator)
+        return "{0}/{1} {2} {3}".format(self.numerator, self.denominator,
+                                        self.negative,
+                                        self.numerator / self.denominator)
 
     def reduce(self):
         """Reduce fraction (divide by greatest common divisor in numerator and
@@ -67,20 +72,29 @@ class Number:
 
     def __add__(self, item):
         if isinstance(item, Number):
+            # Find lcd, calculate adjusted numerators and act accordingly.
             denominator = lcd(self.denominator, item.denominator)
-            res = Number(self.numerator * (denominator // self.denominator) +
-                         item.numerator * (denominator // item.denominator),
-                         denominator)
+            first = self.numerator * (denominator // self.denominator)
+            second = item.numerator * (denominator // item.denominator)
+            if not self.negative and item.negative:
+                numerator = first - second
+            elif self.negative and not item.negative:
+                numerator = second - first
+            else:
+                numerator = first + second
+            res = Number(abs(numerator), denominator)
+            if numerator < 0 or self.negative and item.negative:
+                res.negative = True
+
             res.reduce()
             return res
 
     def __sub__(self, item):
         if isinstance(item, Number):
-            denominator = lcd(self.denominator, item.denominator)
-            res = Number(self.numerator * (denominator // self.denominator) -
-                         item.numerator * (denominator // item.denominator),
-                         denominator)
-            res.reduce()
+            item.negative = not item.negative
+            res = self.__add__(item)
+            item.negative = not item.negative
+
             return res
 
     def __mul__(self, item):
@@ -88,6 +102,7 @@ class Number:
             res = Number(self.numerator * item.numerator,
                          self.denominator * item.denominator)
             res.reduce()
+            res.negative = (self.negative or item.negative)
             return res
 
     def __truediv__(self, item):
@@ -95,31 +110,13 @@ class Number:
             res = Number(self.numerator * item.denominator,
                          self.denominator * item.numerator)
             res.reduce()
+            res.negative = (self.negative or item.negative)
             return res
 
-
-def main():
-    a = Number(1, 2)
-    b = Number(1, 3)
-    print(a)
-    print(b)
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-
-    print()
-    print()
-    print()
-
-    one = Number(1)
-    two = Number(2)
-    eps = Number(1)
-
-    while one + eps/two > one:
-        eps /= two
-        print(eps)
-    print(eps)
-
-
-cProfile.run('main()')
+    def __pow__(self, item):
+        if isinstance(item, int):
+            res = Number(self.numerator ** item, self.denominator ** item)
+            if item % 2 == 1 and self.negative:
+                res.negative = True
+            res.reduce()
+            return res
--- inf_prec_test.py
+++ inf_prec_test.py
@@ -20,6 +20,12 @@ def test_gcd_a_equals_b():
     assert inf_prec.gcd(3, 3) == 3
     assert inf_prec.gcd(7, 7) == 7
 
+
+def test_gcd_negatives():
+    assert inf_prec.gcd(12, -12) == 12
+    assert inf_prec.gcd(3, -3) == 3
+    assert inf_prec.gcd(-7, 7) == 7
+
 # Lowest common denominator.
 
 
@@ -40,6 +46,13 @@ def test_lcd_a_equals_b():
     assert inf_prec.lcd(13, 13) == 13
     assert inf_prec.lcd(9, 9) == 9
 
+
+def test_lcd_negatives():
+    assert inf_prec.lcd(8, -12) == 24
+    assert inf_prec.lcd(-8, 13) == 104
+    assert inf_prec.lcd(8, -9) == 72
+
+
 # Reduce fractions.
 
 
@@ -51,3 +64,115 @@ def test_numerator_bigger_than_denominator():
     b = inf_prec.Number(9, 3)
     b.reduce()
     assert b == inf_prec.Number(3, 1)
+
+    b = inf_prec.Number(9, 3, True)
+    b.reduce()
+    assert b == inf_prec.Number(3, 1, True)
+
+
+def test_numerator_smaller_than_denominator():
+    a = inf_prec.Number(6, 12)
+    a.reduce()
+    assert a == inf_prec.Number(1, 2)
+
+    b = inf_prec.Number(3, 9)
+    b.reduce()
+    assert b == inf_prec.Number(1, 3)
+
+    b = inf_prec.Number(3, 9, True)
+    b.reduce()
+    assert b == inf_prec.Number(1, 3, True)
+
+
+def test_numerator_equals_denominator():
+    a = inf_prec.Number(12, 12)
+    a.reduce()
+    assert a == inf_prec.Number(1, 1)
+
+    b = inf_prec.Number(9, 9)
+    b.reduce()
+    assert b == inf_prec.Number(1, 1)
+
+    b = inf_prec.Number(3, 3, True)
+    b.reduce()
+    assert b == inf_prec.Number(1, 1, True)
+
+
+def test_add():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3)
+    assert a + b == inf_prec.Number(5, 6)
+
+
+def test_add_negative():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3, True)
+    assert a + b == inf_prec.Number(1, 6)
+
+
+def test_add_negatives():
+    a = inf_prec.Number(1, 2, True)
+    b = inf_prec.Number(1, 3, True)
+    assert a + b == inf_prec.Number(5, 6, True)
+
+
+def test_sub():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3)
+    assert a - b == inf_prec.Number(1, 6)
+
+
+def test_sub_negative():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3, True)
+    assert a - b == inf_prec.Number(5, 6)
+
+
+def test_sub_negatives():
+    a = inf_prec.Number(1, 2, True)
+    b = inf_prec.Number(1, 3, True)
+    assert a - b == inf_prec.Number(1, 6, True)
+
+
+def test_mul():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3)
+    assert a * b == inf_prec.Number(1, 6)
+
+
+def test_mul_negative():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3, True)
+    assert a * b == inf_prec.Number(1, 6, True)
+
+
+def test_mul_negatives():
+    a = inf_prec.Number(1, 2, True)
+    b = inf_prec.Number(1, 3, True)
+    assert a * b == inf_prec.Number(1, 6)
+
+
+def test_div():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3)
+    assert a / b == inf_prec.Number(3, 2)
+
+
+def test_div_negative():
+    a = inf_prec.Number(1, 2)
+    b = inf_prec.Number(1, 3, True)
+    assert a / b == inf_prec.Number(3, 2, True)
+
+
+def test_div_negatives():
+    a = inf_prec.Number(1, 2, True)
+    b = inf_prec.Number(1, 3, True)
+    assert a / b == inf_prec.Number(3, 2)
+
+
+def test_pow():
+    a = inf_prec.Number(1, 2)
+    assert a ** 3 == inf_prec.Number(1, 8)
+
+    b = inf_prec.Number(1, 2, True)
+    assert b ** 3 == inf_prec.Number(1, 8, True)
